df938f5099d9b1b8917dddd9192979b85340f1fe
brakil <fantasy.plus@163.com>
2015-08-20 00:28:52 +0800

    Abstract & magic methods.

--- 2015/08/18.php
+++ 2015/08/18.php
@@ -1,25 +1,87 @@
 <?php
 //magic methods
 
-class Person
-{
-    private $prop = array();
-    private $name;
-    private $age;
+// class Person
+// {
+    // private $prop = array();
+    // private $name;
+    // private $age;
     
-    public function __set($key, $value)
+    // public function __set($key, $value)
+    // {
+        // $this->prop[$key] = $value;
+    // }
+    
+    // public function __get($key)
+    // {
+        // return array_key_exists($key, $this->prop) ? $this->prop[$key] :null;
+    // }
+// }
+
+// $p = new Person();
+// $p->name = 'hello world';
+// $p->age = 20;
+
+// var_dump($p->name, $p->age);
+
+
+
+// class Person
+// {
+    // protected $properties = array();
+    
+    // function __construct()
+    // {
+        // $this->properties['name'] = 'foo';
+        // $this->properties['age'] = 20;
+    // }
+    
+    // function __get($key)
+    // {
+        // return array_key_exists($key, $this->properties) ? $this->properties[$key] : null;
+    // }
+// }
+
+// $p = new Person();
+// var_dump($p->name, $p->age);
+
+class Container
+{
+    private $data = array();
+    
+    public function get($key)
     {
-        $this->prop[$key] = $value;
+        return array_key_exists($key, $this->data) ? $this->data[$key] : null;
     }
     
-    public function __get($key)
+    public function set($key, $value)
     {
-        return array_key_exists($key, $this->prop) ? $this->prop[$key] :null;
+        $this->data[$key] = $value;
+    }
+}
+$c = new Container();
+$c->set('name', 'foo');
+$c->set('age', 20);
+
+class parentClass
+{
+    protected $container;
+    
+    function __construct($c)
+    {
+        $this->container = $c;
+    }
+    
+    function __get($key)
+    {
+        return $this->container->get($key);
     }
 }
 
-$p = new Person();
-$p->name = 'hello world';
-$p->age = 20;
+class subClass extends parentClass
+{
+    
+}
 
-var_dump($p->name, $p->age);
+$sc = new subClass($c);
+var_dump($sc->name, $sc->age);
